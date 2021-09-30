a941c00d041f29cb3b782e07424b17c9670a85cd
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-30 17:44:18 +0530

    app bar

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app/utils/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +9,27 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: appBlack,
+      appBar: AppBar(
+        backgroundColor: appBlack,
+        elevation: 0.0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Edit',
+              style: TextStyle(
+                color: appBlue,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(CupertinoIcons.pen),
+              color: appBlue,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
