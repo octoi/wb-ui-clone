0acaf3f3cb7df39a3b734ca948bffff0a1406152
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-30 18:27:09 +0530

    rick image

--- app/lib/utils/constants.dart
+++ app/lib/utils/constants.dart
@@ -5,3 +5,6 @@ const Color appDarkGrey = Color(0xFF171717);
 const Color appGrey = Color(0xFF58595B);
 const Color appWhite = Color(0xFFFAFAFA);
 const Color appBlue = Color(0xFF339DF9);
+
+const String rickImage =
+    'https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png';
